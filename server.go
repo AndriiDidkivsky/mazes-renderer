8b7f10fd9b3b3cdacb1eb12ea4597d60c78dd633
AndriiDidkivsky <Andrii.Didkivsky@gmail.com>
2017-02-14 06:39:01 -0800

    hello server; added reuter.go

--- server.go
+++ server.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+    "net/http"
+    "fmt"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello %s", r.URL.Path[1:])
+}
+
 func main() {
+    fmt.Println("listen localhost:3333")
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":3333", nil)
 
 }
