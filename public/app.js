f747eca2366ed472a52c079f4ef43d7c61e1e9dc
AndriiDidkivsky <andrii.didkivsky@gmail.com>
2017-04-18 23:42:47 +0300

    FE part: init

--- public/app.js
+++ public/app.js
@@ -1,4 +1,25 @@
-window.onload = function() {
-console.log(111)
+(function () { 
+  class MazeRenderer { 
+    constructor (ctx) {
+      if(!ctx) { 
+        throw new Error('Context is required');
+      }
+      this.ctx = ctx;
+    } 
 
-} 
+    draw (maze) {
+
+    } 
+  }
+
+  function init() {
+
+  } 
+
+  window.onload = function() {
+    const canvas = document.querySelector('canvas');
+    const ctx = canvas.getContext('2d');
+    const render = new MazeRenderer(ctx);
+
+  } 
+}())
